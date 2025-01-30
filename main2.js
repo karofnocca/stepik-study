696c56e6f46864affafd258290baea335e4dbc61
karofnocca <vKorkishenko@gmail.com>
2025-01-30 13:05:59 +0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -2,47 +2,73 @@
 
 // alg #1
 var num = [13, 345, 1, 234, 23, 2, 3, 6, 5, 4567, 32, 45, 5, 43, 10];
-const target = 5;
+var target = 5;
 
 function finalSum(num, target) {
-  const seenNumbers = new Set();
-  for (let number of num) {
-    const secondNum = target - number;
-    if (seenNumbers.has(secondNum)) {
-      return [secondNum, number];
+  for (var i = 0; i < num.length; i++) {
+    for (var j = i + 1; j < num.length; j++) {
+      if (num[i] + num[j] === target) {
+        return [num[i], num[j]];
+      }
     }
-    seenNumbers.add(number);
   }
   return null;
 }
 
-finalSum(num, target);
+console.log(finalSum(num, target));
 
-//alg #2 if we need find count of "h"
+//alg #2 if we need find count of any letter
 
-var string = ["Hello my friend! How're you! I am fine, thx"];
+var row = "Hello Michael, chek my hw pls";
+var letter = "l";
 
-function countH(string) {
-  let count = 0;
-  for (let str of string) {
-    for (let char of str) {
-      if (char.toLowerCase() === "h") {
+function countH(row) {
+  if (typeof row !== "string" && !Array.isArray(row)) {
+    return "Это не массив и не строка";
+  }
+  if (Array.isArray(row)) {
+    for (var i = 0; i < row.length; i++) {
+      var str = row[i];
+      if (str.trim() === "") {
+        return "Введена пустая строка";
+      }
+    }
+  } else {
+    if (row.trim() === "") {
+      return "Введена пустая строка";
+    }
+  }
+  var count = 0;
+  if (Array.isArray(row)) {
+    for (var i = 0; i < row.length; i++) {
+      var str = row[i];
+      for (var j = 0; j < str.length; j++) {
+        if (str[j].toLowerCase() === letter) {
+          count++;
+        }
+      }
+    }
+  } else {
+    for (var i = 0; i < row.length; i++) {
+      if (row[i].toLowerCase() === letter) {
         count++;
       }
     }
   }
   return count;
 }
+
+console.log(countH(row));
 
 //alg #2 if we need find count of "space"
 
 var string = ["Hello my friend! How're you! I am fine, thx"];
 
 function countH(string) {
-  let count = 0;
-  for (let str of string) {
-    for (let char of str) {
-      if (char.toLowerCase() === " ") {
+  var count = 0;
+  for (var str of string) {
+    for (var char of str) {
+      if (char === " ") {
         count++;
       }
     }
@@ -50,14 +76,17 @@ function countH(string) {
   return count;
 }
 
+console.log(countH(string));
+
 // alg #3
 
 var arr = [1, 2, 3, 5, 6, 7, 8];
 
 function getArrayLength(arr) {
-  let count = 0;
-  for (let index in arr) {
+  var count = 0;
+  while (count < arr.length) {
     count++;
   }
+  return count;
 }
-getArrayLength(arr);
+console.log(getArrayLength(arr));
